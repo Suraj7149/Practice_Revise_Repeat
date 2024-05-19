c8d89281c1b8ca7a8f5b0b867d94d7cb45f7a4e6
Suraj-Desktop <santoshsuraj7149@gmail.com>
2024-05-19 16:24:53 +0530

    day 13

--- HTML_CSS_JS_projects/Node and Express js projects/mycontacts-backend/server.js
+++ HTML_CSS_JS_projects/Node and Express js projects/mycontacts-backend/server.js
@@ -1,17 +1,18 @@
 const express = require("express");
-const dotenv = require("dotenv").config();
+const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middleware/errorHandler");
-const connectDB = require("./config/dbConnection");
+const dotenv = require("dotenv").config();
 
-connectDB();
+connectDb();
 const app = express();
 
 const port = process.env.PORT || 5000;
+
 app.use(express.json());
-app.use("/api/contacts", require ("./routes/contactsroutes"));
-app.use("/api/users", require ("./routes/usersroutes"));
+app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log("The Server is running on the port : " + port);
+  console.log(`Server running on port ${port}`);
 });
